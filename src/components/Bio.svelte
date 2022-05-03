a012cb2bcfdd3ff277fd771788dee5c0702ef11a
javila35 <josepharthuravila@gmail.com>
2022-05-02 23:19:05 -0700

    initialize react

--- src/components/Bio.svelte
+++
@@ -1,9 +0,0 @@
-<div>
-  <p>Bio here</p>
-</div>
-
-<style>
-  div {
-    grid-area: 4 / 2 / 7 / 8;
-  }
-</style>
--- src/components/ImageBar.svelte
+++
@@ -1,9 +0,0 @@
-<div>
-  <p>images here</p>
-</div>
-
-<style>
-  div {
-    grid-area: 7 / 1 / 11 / 9;
-  }
-</style>
