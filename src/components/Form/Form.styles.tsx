ba3e47639cfd11b3f926ace4e726828d685fef2e
javila35 <josepharthuravila@gmail.com>
2022-05-13 00:58:07 -0700

    send emails

--- src/components/Form/Form.styles.tsx
+++ src/components/Form/Form.styles.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
+  gap: 1em;
 `;
 
 export const InputContainer = styled.div`
